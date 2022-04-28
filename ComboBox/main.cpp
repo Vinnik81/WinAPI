7b9f392df16b7908cd668f54ebc5b605e3f69f06
ВинБоцы ВинБоцы <solarisraver@mail.ru>
2022-04-28 20:33:26 +0600

    ComboBox_HomeWork DONE

--- ComboBox/main.cpp
+++ ComboBox/main.cpp
@@ -1,4 +1,6 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
 #include<Windows.h>
+#include<cstdio>
 #include"resource.h"
 
 CONST CHAR* string[] = { "This", "is", "my", "first", "Combo", "Box" };
@@ -25,6 +27,22 @@ BOOL CALLBACK DlgProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 	}
 		break;
 	case WM_COMMAND:
+		switch (LOWORD(wParam))
+		{
+		case IDOK:
+		{
+			CONST INT SIZE = 256;
+			CHAR sz_buffer[SIZE] = {};
+			HWND hCombo = GetDlgItem(hwnd, IDC_COMBO1);
+			int i = SendMessage(hCombo, CB_GETCURSEL, 0, 0);
+			SendMessage(hCombo, CB_GETLBTEXT, i, (LPARAM)sz_buffer);
+			CHAR sz_msg[SIZE] = {};
+			sprintf(sz_msg, "Вы выбрали элемент № %d, со злначением %s", i, sz_buffer);
+			MessageBox(hwnd, sz_msg, "Info", MB_OK | MB_ICONINFORMATION);
+		}
+			break;
+		case IDCANCEL: EndDialog(hwnd, 0); break;
+		}
 		break;
 	case WM_CLOSE: EndDialog(hwnd, 0);
 	}
